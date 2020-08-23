3f052de9326d48750796a17a2cac51ac6de8e0d3
hector-manny <mejia.martinez2012@hotmail.es>
2020-08-23 11:08:29 -0600

    version 2.01

--- calculadora/calculadora.ts
+++ calculadora/calculadora.ts
@@ -1,1 +1,16 @@
-class
+class calculadora{
+    numero1:number;
+    numero2:number;
+    calculo():number{
+        sum = this.numero1+this.numero2;
+          rest = this.numero1-this.numero2;
+           mult = this.numero1*this.numero2;
+            div = this.numero1/this.numero2;
+    }
+    constructor(num1:number,num2:number) {
+        this.numero1 = num1;
+        this.numero2 = num2;
+    }
+}
+   let calculouno = new calculadora(23, 24);
+   console.log(calculouno.calculo());
